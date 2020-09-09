ea6ff70f285be83b0f038c043f826bc3b5e57dcf
Kars111841 <111841@emmauscollege.nl>
2020-09-09 17:51:43 +0000

    Opruimen, fixing, class.js toegevoegd

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
-var ballSpeedY = 10;
-var ballSpeedX = 10;
+var ballSpeedY = 12;
+var ballSpeedX = 12;
 var ballPosX = 100;
 var ballPosY = 100;
-var glitchFix = false;
+var collisionFix = false;
+var rectPos = 500;
+var hasBounced = false;
+var angleVerandering = 0;
+var bounceCooldown = 0;
 
 function setup() {
     createCanvas(1920, 1080);
 }
-  
-  function draw() {
+
+function draw() {
+    hasBounced = false;
+
     //achtergrond
     background(0, 0, 0);
     noStroke();
 
-    //kan gedelete worden
+    //zijkanten
     fill(128, 128, 128);
     rect(0, 0, 60, 1080);
     rect(0, 0, 1920, 60);
     rect(1860, 0, 60, 1080);
     rect(0, 1020, 1920, 60);
 
-    fill(255, 0 ,0);
+    //controls
+    if (keyIsPressed) {
+        if (keyCode === 37 && rectPos >= 70) {
+            rectPos -= 10;
+        } else if (keyCode === 39 && rectPos <= 1660) {
+            rectPos += 10;
+        }
+    }
+
+    //balk en ball
+    fill(255, 0, 0);
     ellipse(ballPosX, ballPosY, 40, 40);
 
     fill(0, 0, 255);
-    rect(mouseX, 960, 190, 25);
+    rect(rectPos, 960, 190, 25);
 
     //ball physics
-    if(ballPosX >= mouseX -20 && ballPosX <= mouseX + 210 && ballPosY >= 950 && ballPosY <= 965 && glitchFix === false) {
+    if (ballPosX >= rectPos - 20 && ballPosX <= rectPos + 210 && ballPosY >= 950 && ballPosY <= 965 && collisionFix === false) {
         ballSpeedY = ballSpeedY * -1;
-        glitchFix = true;
-    }
+        bounceCooldown = 3;
+        hasBounced = true;
+    } 
 
-    if(ballPosX >= 1840 || ballPosX <= 80) {
+    if (ballPosX >= 1840 || ballPosX <= 80) {
         ballSpeedX = ballSpeedX * -1;
-        glitchFix = false;
+        hasBounced = true;
     }
 
-    if(ballPosY <= 80) {
+    if (ballPosY <= 80) {
         ballSpeedY = ballSpeedY * -1;
-        glitchFix = false; 
+        hasBounced = true;
     }
 
-    if(ballPosY >= 1000) {
+    if (ballPosY >= 1000) {
         textSize(80);
         fill(0, 255, 0);
         text("GAME OVER", 700, 500);
     }
 
+    //ball positite verandering
     ballPosX += ballSpeedX;
     ballPosY += ballSpeedY;
-    
+
+    //ball angle verandering
+    if(hasBounced === true) {
+        angleVerandering = round(random(-6, 6));
+        if(ballSpeedX < 0) {
+            ballSpeedX = -1 * (12 + angleVerandering);
+        } else {
+            ballSpeedX = 12 + angleVerandering;
+        }
+
+        if(ballSpeedY < 0) {
+            ballSpeedY = -1 * (12 + angleVerandering);
+        } else {
+            ballSpeedY = 12 - angleVerandering;
+        }
+        
+        hasBounced = false;
+    }
+
+    bounceCooldown -= 1;
+
 }
